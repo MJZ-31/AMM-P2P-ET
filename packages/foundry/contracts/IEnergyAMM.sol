0e48cf46a8528425fcad571ae0ac5f343fcac521
Mitchel Justinen <mitcheljustinen@proton.me>
2026-06-18 16:42:09 -0600

    Removed the setBidRange and setAskRange functions.
    Reformatted functions so that ETokens are considered the "primary" asset.

--- packages/foundry/contracts/IEnergyAMM.sol
+++ packages/foundry/contracts/IEnergyAMM.sol
@@ -34,8 +34,8 @@ error ZeroTransfer();
 struct TradeInfo {
     address trader;
     string op;
-    uint256 MAmount;
     uint256 EAmount;
+    uint256 MAmount;
     uint256 fee;
     UD60x18 poolPrice;
     UD60x18 tradePrice;
@@ -48,8 +48,8 @@ struct TradeInfo {
 struct LiquidityInfo {
     address provider;
     string op;
-    uint256 MLiq;
     uint256 ELiq;
+    uint256 MLiq;
     uint256 LShare;
     UD60x18 poolPrice;
     UD60x18 liqPrice;
@@ -61,16 +61,16 @@ struct LiquidityInfo {
  */
 interface IEnergyAMM {
     /**
-     * @notice Returns the address of an ERC20 token representing currency.
-     * @return The address of the MToken.
-     */
-    function MToken() external view returns (IERC20);
-
-    /**
      * @notice Returns the address of an ERC20 token representing energy.
      * @return The address of the EToken.
      */
     function EToken() external view returns (IERC20);
+
+    /**
+     * @notice Returns the address of an ERC20 token representing currency.
+     * @return The address of the MToken.
+     */
+    function MToken() external view returns (IERC20);
 
     /**
      * @notice Returns the address of an ERC20 token representing liquidity shares in this market.
@@ -79,20 +79,20 @@ interface IEnergyAMM {
     function LToken() external view returns (IERC20);
 
     /**
-     * @notice Returns the amount of MTokens in the liquidity pool.
-     * @dev The liquidity pool is composed of the tokens owned by the contract itself, so this value will be equivalent
-     * to the amount of MTokens owned by this contract.
-     * @return The amount of MTokens in the liquidity pool.
-     */
-    function MReserve() external view returns (uint256);
-
-    /**
      * @notice Returns the amount of ETokens in the liquidity pool.
      * @dev The liquidity pool is composed of the tokens owned by the contract itself, so this value will be equivalent
      * to the amount of ETokens owned by this contract.
      * @return The amount of ETokens in the liquidity pool.
      */
     function EReserve() external view returns (uint256);
+
+    /**
+     * @notice Returns the amount of MTokens in the liquidity pool.
+     * @dev The liquidity pool is composed of the tokens owned by the contract itself, so this value will be equivalent
+     * to the amount of MTokens owned by this contract.
+     * @return The amount of MTokens in the liquidity pool.
+     */
+    function MReserve() external view returns (uint256);
 
     /**
      * @notice Returns the range of possible values for the pool price.
@@ -127,18 +127,18 @@ interface IEnergyAMM {
     /**
      * @notice Returns the amount of MTokens and ETokens that will be swapped for a bid.
      * @param EAmount The amount of ETokens being bought.
-     * @return MSwap The amount of MTokens that will be transferred from the sender to the liquidity pool for the swap.
      * @return ESwap The amount of ETokens that will be transferred from the liquidity pool to the sender for the swap.
+     * @return MSwap The amount of MTokens that will be transferred from the sender to the liquidity pool for the swap.
      */
-    function bidSwap(uint256 EAmount) external view returns (uint256 MSwap, uint256 ESwap);
+    function bidSwap(uint256 EAmount) external view returns (uint256 ESwap, uint256 MSwap);
 
     /**
      * @notice Returns the amount of MTokens and ETokens that will be swapped for an ask.
      * @param EAmount The amount of ETokens being sold.
-     * @return MSwap The amount of MTokens that will be transferred from the liquidity pool to the sender for the swap.
      * @return ESwap The amount of ETokens that will be transferred from the sender to the liquidity pool for the swap.
+     * @return MSwap The amount of MTokens that will be transferred from the liquidity pool to the sender for the swap.
      */
-    function askSwap(uint256 EAmount) external view returns (uint256 MSwap, uint256 ESwap);
+    function askSwap(uint256 EAmount) external view returns (uint256 ESwap, uint256 MSwap);
 
     /**
      * @notice Returns the amount of MTokens which will be taken as the fee for a bid.
@@ -187,27 +187,27 @@ interface IEnergyAMM {
     /**
      * @notice Returns the amount of LTokens rewarded for a liquidity addition and the amounts of MTokens and ETokens to
      * add.
-     * @param MAmount The amount of MTokens being added to the liquidity pool.
      * @param EAmount The amount of ETokens being added to the liquidity pool.
+     * @param MAmount The amount of MTokens being added to the liquidity pool.
      * @return LShare The amount of LTokens that will be minted and transferred to the sender.
-     * @return MLiq The amount of MTokens that will be transferred from the sender to the liquidity pool. This may be
-     * lower than MAmount to keep the addition price point in the pool price range.
      * @return ELiq The amount of ETokens that will be transferred from the sender to the liquidity pool. This may be
      * lower than MAmount to keep the addition price point in the pool price range.
+     * @return MLiq The amount of MTokens that will be transferred from the sender to the liquidity pool. This may be
+     * lower than MAmount to keep the addition price point in the pool price range.
      */
-    function liquidityProvision(uint256 MAmount, uint256 EAmount)
+    function liquidityProvision(uint256 EAmount, uint256 MAmount)
         external
         view
-        returns (uint256 LShare, uint256 MLiq, uint256 ELiq);
+        returns (uint256 LShare, uint256 ELiq, uint256 MLiq);
 
     /**
      * @notice Returns the amount of MTokens and ETokens to be returned to a liquidity provider for a liquidity removal.
      * @param LAmount The amount of LTokens being given up by the liquidity provider.
      * @return LShare The amount of LTokens that will be transferred from the sender and burned.
-     * @return MLiq The amount of MTokens that will be transferred from the liquidity pool to the sender.
      * @return ELiq The amount of ETokens that will be transferred from the liquidity pool to the sender.
+     * @return MLiq The amount of MTokens that will be transferred from the liquidity pool to the sender.
      */
-    function liquidityReduction(uint256 LAmount) external view returns (uint256 LShare, uint256 MLiq, uint256 ELiq);
+    function liquidityReduction(uint256 LAmount) external view returns (uint256 LShare, uint256 ELiq, uint256 MLiq);
 
     /**
      * @notice Returns the proportion of liquidity shares that the sender holds.
@@ -238,11 +238,11 @@ interface IEnergyAMM {
     /**
      * @notice Executes a liquidity addition. The desired amount of MTokens and ETokens will be transferred from the
      * sender to the liquidity pool. A corresponding amount of LTokens will be minted and transferred to the sender.
-     * @param MAmount The amount of MTokens being added.
-     * @param EAmount The amount of ETokens being added.
+     * @param EAmount The amount of MTokens being added.
+     * @param MAmount The amount of ETokens being added.
      * @return Information about the liquidity addition.
      */
-    function addLiquidity(uint256 MAmount, uint256 EAmount) external returns (LiquidityInfo memory);
+    function addLiquidity(uint256 EAmount, uint256 MAmount) external returns (LiquidityInfo memory);
 
     /**
      * @notice Executes a liquidity removal. The desired amount of LTokens will be transferred from the sender and
@@ -264,16 +264,4 @@ interface IEnergyAMM {
      * @param feeRate The fee rate.
      */
     function setFeeRate(UD60x18 feeRate) external;
-
-    /**
-     * @notice Sets the bid range.
-     * @param range The bid range.
-     */
-    function setBidRange(Range memory range) external;
-
-    /**
-     * @notice Sets the ask range.
-     * @param range The ask range.
-     */
-    function setAskRange(Range memory range) external;
 }
