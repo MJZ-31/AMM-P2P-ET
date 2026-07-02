de832e08234205d006efa77019417f6b014ee77c
Mitchel Justinen <mitcheljustinen@proton.me>
2026-07-02 13:30:17 -0600

    Changed the sizes of numerical types to match the 256-bit integers and 18-point fixed decimals used in the contracts.

--- packages/docker/init.sql
+++ packages/docker/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS AMM_P2P_ET;
 USE AMM_P2P_ET;
 CREATE TABLE IF NOT EXISTS history (
     timestamp DATETIME(0),
-    pool_price REAL(77, 18),
-    EReserve BIGINT,
-    MReserve BIGINT,
-    liquidity BIGINT);
+    pool_price REAL(60, 18),
+    EReserve REAL(78, 0),
+    MReserve REAL(78, 0),
+    liquidity REAL(78, 0));
