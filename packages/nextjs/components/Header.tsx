7255d20e68d3c82dc12d06c2305b6e758aadd061
Mitchel Justinen <mitcheljustinen@proton.me>
2026-07-22 16:04:07 -0600

    Changed website header.

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -22,14 +22,14 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
   {
-    label: "ERC-20",
-    href: "/erc20",
-    icon: <BanknotesIcon className="h-4 w-4" />,
+    label: "Trading",
+    href: "/trade",
+    // icon: <BanknotesIcon className="h-4 w-4" />,
   },
   {
-    label: "Debug Contracts",
-    href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
+    label: "Liquidity",
+    href: "/liquidity",
+    // icon: <BugAntIcon className="h-4 w-4" />,
   },
 ];
 
@@ -88,12 +88,13 @@ export const Header = () => {
           </ul>
         </details>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
-          <div className="flex relative w-10 h-10">
+        {/* <div className="flex relative w-10 h-10">
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
-          </div>
+          </div> */}
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+          {/* <span className="font-bold leading-tight">Scaffold-ETH</span>
+            <span className="text-xs">Ethereum dev stack</span> */}
+            <span className="font-bold leading-tight text-xl">AMM-P2P-ET</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
